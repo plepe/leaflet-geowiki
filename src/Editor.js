b03861b4f26430b42365085c7a3b690f680338f0
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2019-05-28 17:21:23 +0200

    Disable global edit, edit after clicking on shape

--- src/Editor.js
+++ src/Editor.js
@@ -20,7 +20,8 @@ class Editor extends EventEmitter {
     this.map.addLayer(this.items)
     var drawControl = new L.Control.Draw({
       edit: {
-        featureGroup: this.items
+        featureGroup: this.items,
+        edit: false
       }
     })
     this.map.addControl(drawControl)
@@ -28,7 +29,7 @@ class Editor extends EventEmitter {
     this.map.on(L.Draw.Event.CREATED, event => {
       var layer = event.layer
 
-      this.items.addLayer(layer)
+      this.addLayer(layer)
 
       this.emit('change', event)
     })
@@ -41,15 +42,22 @@ class Editor extends EventEmitter {
 
     if (data.type === 'FeatureCollection') {
       data.features.forEach(feature => {
-        L.geoJSON(feature).getLayers().forEach(layer =>
-          this.items.addLayer(layer)
-        )
+        L.geoJSON(feature).getLayers().forEach(layer => this.addLayer(layer))
       })
     } else if (data.type === 'Feature') {
       this.items.addLayer(L.geoJSON(data).getLayers().forEach(layer =>
         this.items.addLayer(layer)
       ))
     }
+  }
+
+  addLayer (layer) {
+    layer.on('click', e => layer.editing.enable())
+    if (layer.setStyle) {
+      layer.setStyle({ editing: {} })
+    }
+
+    this.items.addLayer(layer)
   }
 
   save () {
