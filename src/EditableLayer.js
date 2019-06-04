b331c577effc680b88378ecd38b172bdb8749e14
Stephan Bösch-Plepelits <skunk@cg.tuwien.ac.at>
2019-06-04 09:58:18 +0200

    EditableLayer: edit(), disableEdit()

--- src/EditableLayer.js
+++ src/EditableLayer.js
@@ -1,3 +1,6 @@
+const ModulekitForm = require('modulekit-form')
+
+const getLayerForm = require('./getLayerForm')
 const Layer = require('./Layer')
 const EditableFeature = require('./EditableFeature')
 
@@ -19,6 +22,60 @@ class EditableLayer extends Layer {
     return item
   }
 
+  edit () {
+    this.editor.disableCurrentEditing()
+
+    this.editor.currentEdit = this
+
+    this.editor.sidebarDom.innerHTML = ''
+
+    let form = document.createElement('form')
+    this.editor.sidebarDom.appendChild(form)
+    form.onsubmit = () => {
+      this.editor.edit()
+      return false
+    }
+
+    let f = new ModulekitForm(
+      'data',
+      getLayerForm(), {
+        change_on_input: true
+      }
+    )
+
+    f.show(form)
+
+    let input = document.createElement('input')
+    input.type = 'submit'
+    input.value = 'Ok'
+    form.appendChild(input)
+
+    f.set_data({
+      properties: this.properties,
+      style: this.style
+    })
+
+    f.onchange = () => {
+      let newData = f.get_data()
+
+      for (let k in newData.properties) {
+        this.properties[k] = newData.properties[k]
+      }
+      for (let k in newData.style) {
+        if (newData.style[k] === null) {
+          delete this.style[k]
+        } else {
+          this.style[k] = newData.style[k]
+        }
+      }
+
+      applyStyle(this.leafletLayer, this.style)
+    }
+  }
+
+  disableEdit () {
+    this.editor.sidebarDom.innerHTML = ''
+  }
 }
 
 module.exports = EditableLayer
