3ccf2af0bab9c8593f96efc2e2a5e500fb711228
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2019-06-10 14:24:13 +0200

    listLayers: use vue.js for rendering text; improve style

--- src/listLayers.js
+++ src/listLayers.js
@@ -1,28 +1,15 @@
-function showLayer (layer) {
+const Vue = require('vue')
+
+function showLayer (data) {
   let div = document.createElement('li')
-  let actions = document.createElement('span')
-  actions.className = 'actions'
-  div.appendChild(actions)
 
-  let a = document.createElement('a')
-  a.innerHTML = '<i class="fas fa-eye"></i>'
-  a.href = '#'
-  a.onclick = () => {
-    layer.toggleVisibility()
-    return false
-  }
-  actions.appendChild(a)
+  let el = document.createElement('div')
+  el.innerHTML = '<div v-bind:class="{ hidden: isHidden }"><span class="actions"><a href="#" v-on:click="$data.toggleVisibility()" class="toggle-vis"></a></span><a href="#" v-on:click="$data.edit()">{{ $data.name() }}</a></div>'
+  div.appendChild(el)
 
-  a = document.createElement('a')
-  a.appendChild(document.createTextNode(layer.name()))
-  a.href = '#'
-  a.onclick = () => {
-    layer.edit()
-    return false
-  }
-  div.appendChild(a)
+  new Vue({ el, data })
 
-  let subLayers = listLayers(layer)
+  let subLayers = listLayers(data)
   if (subLayers) {
     div.appendChild(subLayers)
   }
@@ -36,6 +23,7 @@ function listLayers (layer) {
   }
 
   let ul = document.createElement('ul')
+  ul.className = 'leaflet-geowiki'
 
   layer.layerTree.forEach(subLayer => {
     let li = showLayer(subLayer)
