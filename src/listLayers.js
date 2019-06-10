3cf96860ee58c99576fb4465acf464ce77dad9e9
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2019-06-10 14:44:48 +0200

    listLayers: Create a html file for vue template

--- src/listLayers.js
+++ src/listLayers.js
@@ -1,10 +1,11 @@
+const fs = require('fs')
 const Vue = require('vue')
 
 function showLayer (data) {
   let div = document.createElement('li')
 
   let el = document.createElement('div')
-  el.innerHTML = '<div v-bind:class="{ hidden: isHidden }"><span class="actions"><a href="#" v-on:click="$data.toggleVisibility()" class="toggle-vis"></a></span><a href="#" v-on:click="$data.edit()">{{ $data.name() }}</a></div>'
+  el.innerHTML = fs.readFileSync(__dirname + '/listLayers.html', 'utf8')
   div.appendChild(el)
 
   new Vue({ el, data })
